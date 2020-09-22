bf00f1f6d2ff15fbb5976e06ffdfa2c5cb483c58
PN <PN@pn.pl>
2020-09-22 23:12:19 +0200

    change print msg

--- simpleweb/index.js
+++ simpleweb/index.js
@@ -3,7 +3,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("lols");
+  res.send("Hello there, how are you doing??");
 });
 
 app.listen(8080, () => {
